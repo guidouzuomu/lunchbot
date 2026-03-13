2e2b8f9b75fe9af209deaf51a572e98b4b5137a4
guidouzuomu <guidouzuomu@gmail.com>
2026-03-14 06:55:16 +0900

    返信のセクション対応

--- commands/utility/lunch.js
+++ commands/utility/lunch.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, ContainerBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ButtonStyle, MessageFlags } from 'discord.js';
 import fs from 'fs';
 import 'dotenv/config';
 
@@ -6,15 +6,44 @@ import 'dotenv/config';
 export const data = new SlashCommandBuilder().setName('lunch').setDescription('Provide a random restaurant pick!');
 
 export async function execute(interaction) {
-    await interaction.reply('検索中');
-    const address = '福島県会津若松市'
-    const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&address=${encodeURIComponent(address)}&format=json`
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.results);
-    const randomIndex=Math.floor(Math.random() * data.results.shop.length);
-    console.log(data.results.shop[randomIndex].name);
-    await interaction.editReply(`${data.results.shop[randomIndex].name}`)
+    const container = new ContainerBuilder()
+        .setAccentColor(0x0099ff)
+        .addTextDisplayComponents((textDisplay) =>
+            textDisplay.setContent(
+                'エリアを選択してください'
+            ),
+        )
+        .addActionRowComponents((actionRow) =>
+            actionRow.addComponents(new StringSelectMenuBuilder()
+                .setCustomId('area')
+                .setPlaceholder('エリアを選択してください')
+                .addOptions(
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel('駅周辺')
+                        .setDescription('aaa')
+                        .setValue('X949'),
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel('駅以外')
+                        .setDescription('bbb')
+                        .setValue('XLI5'),
+                )
+            ),
+        )
+        .addSeparatorComponents((separator) => separator)
+        .addSectionComponents((section) =>
+            section
+                .addTextDisplayComponents((textDisplay) =>
+                    textDisplay.setContent('discription')
+                )
+                .setButtonAccessory((button) =>
+                    button.setCustomId('send').setLabel('送信').setStyle(ButtonStyle.Primary),
+                ),
+        )
+
+    await interaction.reply({
+        components: [container],
+        flags: MessageFlags.IsComponentsV2,
+    });
 }
 
 
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,103 +1,147 @@
-import { Events, Collection, MessageFlags } from 'discord.js';
+import { Events, Collection, MessageFlags, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, ContainerBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
+import fs from 'fs';
+import 'dotenv/config';
 
-export const name=Events.InteractionCreate;
-    export async function  execute(interaction) {
-        if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
-            const command = interaction.client.commands.get(interaction.commandName);
+const userSelectArea = new Map();
 
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
+export const name = Events.InteractionCreate;
+export async function execute(interaction) {
+    if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        // if (interaction.isAutocomplete()) {
+        //     if (!command) {
+        //         console.error(`No command matching ${interaction.commandName} was found.`);
+        //         return;
+        //     }
+        //     try {
+        //         await command.autocomplete(interaction);
+        //     } catch (error) {
+        //         console.error(error);
+        //     }
+        // }
+
+        // if (interaction.isUserContextMenuCommand()) {
+        //     // ここでコマンドを呼び出す
+        // }
+
+        const { cooldowns } = interaction.client;
+        if (!cooldowns.has(command.data.name)) {
+            cooldowns.set(command.data.name, new Collection());
+        }
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.data.name);
+        const defaultCooldownDuration = 3;
+        const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+        if (timestamps.has(interaction.user.id)) {
+            const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+            if (now < expirationTime) {
+                const expiredTimestamp = Math.round(expirationTime / 1_000);
+                return interaction.reply({
+                    content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+                    flags: MessageFlags.Ephemeral,
+                });
+            }
+        }
+
+        timestamps.set(interaction.user.id, now);
+        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    flags: MessageFlags.Ephemeral,
+                });
+            } else {
+                await interaction.reply({
+                    content: 'There was an error while executing this command!',
+                    flags: MessageFlags.Ephemeral,
+                });
+            }
+        }
+
+
+        // } else if (interaction.isModalSubmit()) {
+        //     if (interaction.customId === 'myModal') {
+        //         await interaction.reply({ content: 'Your submission was received successfully!' });
+        //         // Get the data entered by the user
+        //         // const pong=interaction.fields.getTextInputValue('pong');
+        //         // const hobbies = interaction.fields.getTextInputValue('hobbiesInput');
+        //         const starter = interaction.fields.getStringSelectValues('starter');
+        //         // const picture = interaction.fields.getUploadedFiles('picture');
+        //         // console.log({ pong,hobbies, starter, picture });
+        //         console.log({ starter });
+
+        //     }
+    } else if (interaction.isStringSelectMenu()) {
+        if (interaction.customId === 'area') {
+            userSelectArea.set(interaction.user.id, interaction.values[0]);
+            await interaction.deferUpdate();
+            console.log(interaction.values[0]);
+            return;
+        }
+    } else if (interaction.isButton()) {
+        if (interaction.customId === 'send') {
+            const userSelect = userSelectArea.get(interaction.user.id);
+
+            if (!userSelect) {
+                return await interaction.reply('エリアを選択してください');
             }
 
-            // if (interaction.isAutocomplete()) {
-            //     if (!command) {
-            //         console.error(`No command matching ${interaction.commandName} was found.`);
-            //         return;
-            //     }
-            //     try {
-            //         await command.autocomplete(interaction);
-            //     } catch (error) {
-            //         console.error(error);
-            //     }
-            // }
 
-            // if (interaction.isUserContextMenuCommand()) {
-            //     // ここでコマンドを呼び出す
-            // }
+            const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&small_area=${userSelect}&format=json`
 
-            const { cooldowns } = interaction.client;
-            if (!cooldowns.has(command.data.name)) {
-                cooldowns.set(command.data.name, new Collection());
-            }
-            const now = Date.now();
-            const timestamps = cooldowns.get(command.data.name);
-            const defaultCooldownDuration = 3;
-            const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
-            if (timestamps.has(interaction.user.id)) {
-                const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-                if (now < expirationTime) {
-                    const expiredTimestamp = Math.round(expirationTime / 1_000);
-                    return interaction.reply({
-                        content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
-                        flags: MessageFlags.Ephemeral,
-                    });
-                }
-            }
+            const response = await fetch(url);
+            const data = await response.json();
 
-            timestamps.set(interaction.user.id, now);
-            setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+            const randomIndex = Math.floor(Math.random() * data.results.shop.length);
+            const description1 = data.results.shop[randomIndex].shop_detail_memo || '';
+            const description2 = data.results.shop[randomIndex].close || '';
 
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({
-                        content: 'There was an error while executing this command!',
-                        flags: MessageFlags.Ephemeral,
-                    });
-                } else {
-                    await interaction.reply({
-                        content: 'There was an error while executing this command!',
-                        flags: MessageFlags.Ephemeral,
-                    });
-                }
-            }
+            const description = description1 + description2;
+            const replyEmbed = new EmbedBuilder()
+                .setAuthor({
+                    name: 'Powered by ホットペッパーグルメ Webサービス',
+                    url: 'http://webservice.recruit.co.jp/'
+                })
+                .setColor(0x0099ff)
+                .setTitle(data.results.shop[randomIndex].name)
+                .setImage(data.results.shop[randomIndex].logo_image)
+                .setDescription('定休日:' + data.results.shop[randomIndex].close + '\n' + data.results.shop[randomIndex].shop_detail_memo)
+                .setURL(data.results.shop[randomIndex].urls.pc)
+                .setFooter({ text: '画像提供：ホットペッパー グルメ' });
 
+            console.log('name:' + data.results.shop[randomIndex].name + '\n shopmemo:' + data.results.shop[randomIndex].shop_detail_memo +
+                '\n close:' + data.results.shop[randomIndex].close
+            );
 
-            // } else if (interaction.isModalSubmit()) {
-            //     if (interaction.customId === 'myModal') {
-            //         await interaction.reply({ content: 'Your submission was received successfully!' });
-            //         // Get the data entered by the user
-            //         // const pong=interaction.fields.getTextInputValue('pong');
-            //         // const hobbies = interaction.fields.getTextInputValue('hobbiesInput');
-            //         const starter = interaction.fields.getStringSelectValues('starter');
-            //         // const picture = interaction.fields.getUploadedFiles('picture');
-            //         // console.log({ pong,hobbies, starter, picture });
-            //         console.log({ starter });
-
-            //     }
-            // } else if (interaction.isStringSelectMenu()) {
-            //     if (interaction.customId === 'starter') {
-            //         await interaction.reply({ content: 'Your submission was received successfully!' });
-            //         const starter = interaction.values[0];
-            //         console.log({ starter });
-
-
-            //     }
-            // }else if (interaction.isUserSelectMenu()) {
-            //     if (interaction.customId === 'users') {
-            //         // 選択されたユーザーたちのIDを取得
-            //         const userIds = interaction.values; 
-            //         // メンション形式にして表示
-            //         const mentions = userIds.map(id => `<@${id}>`).join(', ');
-
-            //         await interaction.reply({ 
-            //             content: `選択されたユーザー: ${mentions}`,
-            //             flags: MessageFlags.Ephemeral 
-            //         });
-            //     }
-            // }
+            await interaction.reply({
+                embeds: [replyEmbed]
+            })
         }
     }
+}
+// }else if (interaction.isUserSelectMenu()) {
+//     if (interaction.customId === 'users') {
+//         // 選択されたユーザーたちのIDを取得
+//         const userIds = interaction.values;
+//         // メンション形式にして表示
+//         const mentions = userIds.map(id => `<@${id}>`).join(', ');
+
+//         await interaction.reply({
+//             content: `選択されたユーザー: ${mentions}`,
+//             flags: MessageFlags.Ephemeral
+//         });
+//     }
+// }
+
